7ffc61dd01ce533705f482b1b8d9f69bd778231f
0xys <0xys1@proton.me>
2023-12-19 21:22:29 +0900

    creating client

--- main.go
+++ main.go
@@ -122,13 +122,73 @@ func tx_origin(origin *uint8)
 
 type Bytes []uint8
 
+func (b Bytes) String() string {
+	dst := make([]byte, EncodedLen(len(b)))
+	Encode(dst, b)
+	return string(dst)
+}
+
 const AddressLen = 20
 
 type Address [AddressLen]uint8
 
 func (a Address) String() string {
-	//return hex.EncodeToString([]byte(a[:]))
-	return "todo"
+	dst := make([]byte, EncodedLen(len(a[:])))
+	Encode(dst, a[:])
+	return string(dst)
+}
+
+func (a Address) Balance() Bytes {
+	bal := make([]byte, 32, 32)
+	account_balance(&a[0], &bal[0])
+	return bal
+}
+func (a Address) CodeHash() Bytes {
+	hash := make([]byte, 32, 32)
+	account_codehash(&a[0], &hash[0])
+	return hash
+}
+
+func BlockBaseFee() Bytes {
+	bal := make([]byte, 32, 32)
+	block_basefee(&bal[0])
+	return bal
+}
+func ChainID() uint64 {
+	return chainid()
+}
+func BlockCoinbase() Address {
+	addr := make([]uint8, AddressLen, AddressLen)
+	block_coinbase(&addr[0])
+	return Address(addr)
+}
+func BlockGasLimit() uint64 {
+	return block_gas_limit()
+}
+func BlockNumber() uint64 {
+	return block_number()
+}
+func BlockTimestamp() uint64 {
+	return block_timestamp()
+}
+
+// https://go.googlesource.com/go/+/go1.9.2/src/encoding/hex/hex.go
+const hextable = "0123456789abcdef"
+
+// EncodedLen returns the length of an encoding of n source bytes.
+// Specifically, it returns n * 2.
+func EncodedLen(n int) int { return n * 2 }
+
+// Encode encodes src into EncodedLen(len(src))
+// bytes of dst. As a convenience, it returns the number
+// of bytes written to dst, but this value is always EncodedLen(len(src)).
+// Encode implements hexadecimal encoding.
+func Encode(dst, src []byte) int {
+	for i, v := range src {
+		dst[i*2] = hextable[v>>4]
+		dst[i*2+1] = hextable[v&0x0f]
+	}
+	return len(src) * 2
 }
 
 func GetCalldata(length uint32) Bytes {
@@ -155,65 +215,121 @@ func Log(msg string) {
 
 func LogN(msg string, topics uint32) {
 	bytes := []byte(msg)
+	LogRawN(bytes, topics)
+}
+func LogRawN(bytes Bytes, topics uint32) {
 	length := len(bytes)
 	emit_log(&bytes[0], uint32(length), topics)
 }
 
+func ReturnBytes(bytes Bytes) {
+	write_result(&bytes[0], uint32(len(bytes)))
+}
+func ReturnUInt8(val uint8) {
+	bytes := []uint8{val}
+	ReturnBytes(bytes)
+}
+func ReturnString(msg string) {
+	ReturnBytes([]byte(msg))
+}
+
+// big-endian
+func ReturnUInt64(v uint64) {
+	b := make([]uint8, 8, 8)
+	b[0] = byte(v >> 56)
+	b[1] = byte(v >> 48)
+	b[2] = byte(v >> 40)
+	b[3] = byte(v >> 32)
+	b[4] = byte(v >> 24)
+	b[5] = byte(v >> 16)
+	b[6] = byte(v >> 8)
+	b[7] = byte(v)
+	ReturnBytes(b)
+}
+
+func ReturnAddress(addr Address) {
+	ReturnBytes(addr[:])
+}
+
+func SStore(key, value Bytes) {
+	storage_store_bytes32(&key[0], &value[0])
+}
+func SLoad(key Bytes) Bytes {
+	ret := make([]uint8, 32, 32)
+	storage_load_bytes32(&key[0], &ret[0])
+	return ret
+}
+
 //export user_entrypoint
 func user_entrypoint(args_len uint32) uint32 {
-	addr := []uint8{0}
-	dest := []uint8{0}
-	val := []uint8{0}
-	ll := []uint32{0}
-	account_balance(&addr[0], &dest[0])
-	account_codehash(&addr[0], &dest[0])
+	Log("hello")
+	Log(TxOrigin().String())
+	calldata := GetCalldata(20)
 
-	GetCalldata(args_len)
-	TxOrigin()
-	Log("hello world")
+	addr := Address(calldata)
+	LogRawN(addr[:], 2)
 
-	storage_load_bytes32(&addr[0], &dest[0])
-	storage_store_bytes32(&addr[0], &dest[0])
+	LogRawN(addr.Balance(), 3)
+	LogRawN(MsgSender().Balance(), 3)
 
-	block_basefee(&val[0])
+	ReturnUInt64(BlockNumber())
 
-	chainid()
+	/*
+		addr := []uint8{0}
+		dest := []uint8{0}
+		val := []uint8{0}
+		ll := []uint32{0}
+		account_balance(&addr[0], &dest[0])
+		account_codehash(&addr[0], &dest[0])
 
-	block_coinbase(&addr[0])
-	block_gas_limit()
-	block_number()
-	block_timestamp()
+		GetCalldata(args_len)
+		TxOrigin()
+		Log("hello world")
 
-	call_contract(&addr[0], &dest[0], uint32(10), &val[0], uint64(11), &ll[0])
-	contract_address(&addr[0])
-	create1(&val[0], uint32(10), &addr[0], &addr[0], &ll[0])
-	create2(&val[0], uint32(10), &addr[0], &val[0], &addr[0], &ll[0])
-	delegate_call_contract(&addr[0], &dest[0], uint32(10), uint64(11), &ll[0])
+		storage_load_bytes32(&addr[0], &dest[0])
+		storage_store_bytes32(&addr[0], &dest[0])
 
-	emit_log(&val[0], uint32(10), uint32(11))
+		block_basefee(&val[0])
 
-	evm_gas_left()
-	evm_ink_left()
+		chainid()
 
-	memory_grow(0)
+		block_coinbase(&addr[0])
+		block_gas_limit()
+		block_number()
+		block_timestamp()
 
-	msg_sender(&addr[0])
-	msg_value(&val[0])
+		call_contract(&addr[0], &dest[0], uint32(10), &val[0], uint64(11), &ll[0])
+		contract_address(&addr[0])
+		create1(&val[0], uint32(10), &addr[0], &addr[0], &ll[0])
+		create2(&val[0], uint32(10), &addr[0], &val[0], &addr[0], &ll[0])
+		delegate_call_contract(&addr[0], &dest[0], uint32(10), uint64(11), &ll[0])
 
-	native_keccak256(&addr[0], uint32(10), &dest[0])
 
-	read_args(&val[0])
-	read_return_data(&dest[0], uint32(10), uint32(32))
-	write_result(&dest[0], uint32(10))
-	return_data_size()
-	static_call_contract(&addr[0], &dest[0], uint32(10), uint64(11), &ll[0])
-	tx_gas_price(&val[0])
-	tx_ink_price()
-	tx_origin(&addr[0])
+		emit_log(&val[0], uint32(10), uint32(11))
 
+		evm_gas_left()
+		evm_ink_left()
+
+		memory_grow(0)
+
+		msg_sender(&addr[0])
+		msg_value(&val[0])
+
+		native_keccak256(&addr[0], uint32(10), &dest[0])
+
+		read_args(&val[0])
+		read_return_data(&dest[0], uint32(10), uint32(32))
+		write_result(&dest[0], uint32(10))
+		return_data_size()
+		static_call_contract(&addr[0], &dest[0], uint32(10), uint64(11), &ll[0])
+		tx_gas_price(&val[0])
+		tx_ink_price()
+		tx_origin(&addr[0])
+	*/
 	return 0
 }
 
 func main() {
+	memory_grow(0)
 	user_entrypoint(1)
 }
---
+++ tools/index.js
@@ -0,0 +1,55 @@
+const ethers = require('ethers');
+
+const addr = '0x1f1aebcA347A03218bEDEe7173D150db1d3c5388';
+const coder = new ethers.AbiCoder();
+
+//
+/**
+ * 
+ * @param {string} sig e.g. 'transfer(address,uint256)'
+ * @returns string
+ */
+const funcSignature = (sig) => {
+    return ethers.id(sig).substring(0,10)
+}
+
+const call = async (provider) => {
+    const returnData = await provider.call({
+        to: '0xAddress',
+        data: ethers.concat([
+            funcSignature('transfer(address,uint256)'), 
+            coder.encode(['address', 'uint256'], ['0x1f1aebcA347A03218bEDEe7173D150db1d3c5388', '0x11'])
+        ])
+    })
+    return returnData;
+}
+
+(async () => {
+    const privateKey = process.env.ETH_PRIVATE_KEY;
+    const destAddr = '0xE078Cdb3356D1151d30C1a2F3e2F22F2eFB0b011'
+    
+    const provider = new ethers.JsonRpcProvider(`https://stylus-testnet.arbitrum.io/rpc`);
+    const wallet = new ethers.Wallet(privateKey, provider)
+
+    // const a = ethers.concat([
+    //     funcSignature('transfer(address,uint256)'), 
+    //     coder.encode(['address', 'uint256'], ['0x1f1aebcA347A03218bEDEe7173D150db1d3c5388', '0x11'])
+    // ])
+
+    const tx = {
+        to: destAddr,
+        gasPrice: ethers.parseUnits('100', 'mwei'),
+        gasLimit: 2000000,
+        data: '0xa4b000000000000000000073657175656e636572'
+    };
+
+    const res = await wallet.sendTransaction(tx);
+    console.log(res.hash);
+    console.log(res);
+    // const balanceInWei = await provider.getBalance(addr);
+
+    // // Convert the balance to a more readable format (Ether)
+    // const balanceInEther = ethers.formatEther(balanceInWei);
+    // // Print the balance
+    // console.log(`Your account balance is: ${balanceInEther} ETH`);
+})()
